a51c2d77316a9df6a9451c54a53368a04e6ea2d0
keyur kayda <keyur.kyada@corenettechltd.com>
2026-06-02 11:25:31 +0530

    admin inital commit

---
+++ src/features/product/productService.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { config } from "../../utils/axiosconfig";
+import { base_url } from "../../utils/baseUrl";
+
+const getProducts = async () => {
+  const response = await axios.get(`${base_url}product/`);
+
+  return response.data;
+};
+const createProduct = async (product) => {
+  const response = await axios.post(`${base_url}product/`, product, config);
+
+  return response.data;
+};
+
+const getProduct = async (id) => {
+  const response = await axios.get(`${base_url}product/${id}`, config);
+
+  return response.data;
+};
+
+const updateProduct = async (product) => {
+  const color = product?.productData?.color;
+  if (!color || (Array.isArray(color) && color.length === 0)) {
+    throw new Error("Pick at least one color");
+  }
+  const response = await axios.put(
+    `${base_url}product/${product.id}`,
+    {
+      title: product.productData.title,
+      description: product.productData.description,
+      price: product.productData.price,
+      brand: product.productData.brand,
+      quantity: product.productData.quantity,
+      category: product.productData.category,
+      tags: product.productData.tags,
+      color: color,
+      images: product.productData.images,
+    },
+    config
+  );
+
+  return response.data;
+};
+
+const deleteproduct = async (id) => {
+  const response = await axios.delete(`${base_url}product/${id}`, config);
+
+  return response.data;
+};
+
+const productService = {
+  getProducts,
+  createProduct,
+  deleteproduct,
+  updateProduct,
+  getProduct,
+};
+
+export default productService;
